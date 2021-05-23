ad70bda2a39a613fa0f60b4e447aca4953ced9a2
facundo <facundovigo@gmail.com>
2021-05-22 22:07:30 -0300

    Se agrega sonidos

---
+++ clase7/begin/entities/player/PlayerOutput.gd
@@ -0,0 +1,6 @@
+extends AudioStreamPlayer 
+
+onready var jump = load("res://Audio/Sfx/Jump.ogg")
+onready var roll = load("res://Audio/Sfx/Roll.ogg") 
+onready var footsteps = [load("res://Audio/Sfx/Leaves1.ogg"), load("res://Audio/Sfx/Leaves2.ogg")]
+onready var dead = load("res://Audio/Bgm/gameOver.ogg")
